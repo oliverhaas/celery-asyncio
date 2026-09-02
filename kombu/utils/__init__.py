d563e48cfa11cb910b822ebdc5af5dee1c5b09cc
Oliver Haas <ohaas@e1plus.de>
2026-09-02 14:09:25 +0200

    refactor(utils): stop re-exporting from kombu.utils
    The package docstring has called these imports deprecated since kombu 4,
    and every caller already imports from the module that defines the name.
    `symbol_by_name` is the exception celery still imports from here.

--- kombu/utils/__init__.py
+++ kombu/utils/__init__.py
@@ -1,19 +1,7 @@
 # Originally from Kombu by Ask Solem & contributors (BSD-3-Clause)
 # https://github.com/celery/kombu
-"""DEPRECATED - Import from modules below."""
+"""Utilities."""
 
-from .functional import fxrange, maybe_list, reprcall, reprkwargs, retry_over_time
 from .imports import symbol_by_name
-from .objects import cached_property
-from .uuid import uuid
 
-__all__ = (
-    "cached_property",
-    "fxrange",
-    "maybe_list",
-    "reprcall",
-    "reprkwargs",
-    "retry_over_time",
-    "symbol_by_name",
-    "uuid",
-)
+__all__ = ("symbol_by_name",)
