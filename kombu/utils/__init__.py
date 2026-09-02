4fff8199709ba8300619f9779414e3284b3f460a
Oliver Haas <ohaas@e1plus.de>
2026-09-02 13:59:49 +0200

    refactor: remove the unused collection helpers
    `HashedSeq`, `eqhash` and `EqualityDict` had no caller and no test.
    `EqualityDict` was re-exported from `kombu.utils`; nothing imported it
    from there either.

--- kombu/utils/__init__.py
+++ kombu/utils/__init__.py
@@ -2,7 +2,6 @@
 # https://github.com/celery/kombu
 """DEPRECATED - Import from modules below."""
 
-from .collections import EqualityDict
 from .div import emergency_dump_state
 from .functional import fxrange, fxrangemax, maybe_list, reprcall, reprkwargs, retry_over_time
 from .imports import symbol_by_name
@@ -10,7 +9,6 @@ from .objects import cached_property
 from .uuid import uuid
 
 __all__ = (
-    "EqualityDict",
     "cached_property",
     "emergency_dump_state",
     "fxrange",
